8a19fa399f518971fd3180e8c4ef79fe2efd4e0f
Amit K Rao <amitraok@fastmail.in>
2013-08-11 16:20:47 -0700

    milestone 2

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,4 +1,6 @@
 class DiscountCalculator(object):
 
-    def calculate(self, total, discount_amount, discount_type):
-        return 10.0
+    def calculate(self, total, discount, discount_type):
+        discount_percent = float(discount)/100
+        discount_amount = float(total) * discount_percent
+        return discount_amount
